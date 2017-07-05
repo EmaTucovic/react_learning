436759a1e208874138f832fab6b46542e2c61c98
Emilija <emilija.tucovic@ezderm.com>
2017-07-05 16:17:59 +0200

    Putting fetched result in map by searcTerm

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -30,10 +30,12 @@ export class List extends React.Component {
 	constructor(){
 		super();
 
+		//In order to have a client cache for each result, you have to store multiple results rather than one result in your internal component state.
+		// The results object will be a map with the search term as key and the result as value.
 		this.state = {
-			list: list,
 			searchTerm : DEFAULT_QUERY,
-			result: null
+			results: null, //[{ hits: [], page : numb}, {...}]
+			searchKey: '' // it reflects "searchTerm"
 		};
 
 		this.onDismiss = this.onDismiss.bind(this);
@@ -48,19 +50,18 @@ export class List extends React.Component {
 	//Only that way you can sneak in the objectID property. Otherwise you would have to define the function outside.
 
 	onDismiss(id){
-		console.log("on dismiss");
-		console.log(this.state.result);
-		//Now the response is complex object with one property hits that is of interest!
-		// we never mutate the state like this :
-		//this.state.result.hits = updatedList;
-		//We create new obj like this:
-		const updatedHits = this.state.result.hits.filter( (item) => item.objectID !== id );
-		console.log(updatedHits);
+		const {searchKey, results} = this.state;
+		const {hits, page} = results[searchKey];
+		//We never mutate state obj, but create new obj like this:
+		const updatedHits = hits.filter( (item) => item.objectID !== id );
+
 		this.setState ( {
 			//result : Object.assign( {}, this.state.result, {hits: updatedHits})
-			result : {...this.state.result, hits : updatedHits}
+			results : {
+				...results,
+				[searchKey]: { hits : updatedHits, page}
+			}
 		});
-		console.log(this.state.result);
 	}
 
 	onSearchChange(event){
@@ -78,8 +79,9 @@ export class List extends React.Component {
 
 	render(){
 		//Destructuring
-		const { searchTerm , result} = this.state; 
-		const page = (result && result.page) || 0;
+		const { searchTerm , searchKey, results} = this.state; 
+		const page = (results && results[searchKey] && results[searchKey].page) || 0;
+		const list = ( results && results[searchKey] && results[searchKey].hits) || [];
 		//console.log(result); //Result is an object with property hits that is array of interest
 		
 		//Key should be specified inside the array
@@ -91,8 +93,7 @@ export class List extends React.Component {
 				Search
 			</Search>
 			<ol>
-			{result 
-				? result.hits.map( (item)=>
+			{list.map( (item)=>
 					<li key = {item.objectID}>
 						<span>
 						<a href={item.url}>{item.title}</a>
@@ -102,18 +103,18 @@ export class List extends React.Component {
 					<span>{item.points}</span>
 					<Button onClick = {()=> this.onDismiss(item.objectID)} > Dismiss </Button>
 					</li>) 
-				: null
 			}
 			</ol>
-			<Button onClick = {() => this.fetchSearchTopStories(searchTerm, page +1)} > 
+			<Button onClick = {() => this.fetchSearchTopStories(searchKey, page +1)} > 
 				More
 			</Button>
 		</div>
 		);
 	}
 
-	componentWillMount(){
+	componentDidMount(){
 		const {searchTerm} = this.state;
+		this.setState({searchKey : searchTerm});
 		this.fetchSearchTopStories(searchTerm, DEFAULT_PAGE);
 	}
 
@@ -133,9 +134,10 @@ export class List extends React.Component {
 
 	setSearchTopstories(result) {
 		const { hits, page } =  result;
+		const { searchKey, results } = this.state;
 
-		const oldHits = page !== 0
-			? this.state.result.hits
+		const oldHits = results && results[searchKey]
+			? results[searchKey] = hits
 			: [];
 
 		const updatedHits = [
@@ -144,15 +146,22 @@ export class List extends React.Component {
 		];
 
 		this.setState( {
-			result : {  ...result, hits : hits, page}
+			results : { 
+				...results,//all other results by searchkey
+				//[searchKey] is an ES6 computed property name. It helps you to allocate values dynamically in an object.
+				[searchKey] :{ hits : updatedHits, page }
+			}
 		});
+		console.log(this.state.results);
 	}
 
 	handleSearchSubmit(event) {
+		const {searchTerm} = this.state;
 		//You must do this because native browser behaviour for submit callback in form is to relload!
 		//If you do not do this the page will always reload and start from the scratch 
 		event.preventDefault(); 
-		this.fetchSearchTopStories(this.state.searchTerm, DEFAULT_PAGE);
+		this.setState( { searchKey : searchTerm} );
+		this.fetchSearchTopStories(searchTerm, DEFAULT_PAGE);
 	}
 }
 
