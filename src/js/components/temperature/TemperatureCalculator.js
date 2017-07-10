73691ec35576be36614b7b5999750c5ec1268f04
Emilija <emilija.tucovic@ezderm.com>
2017-07-10 13:53:02 +0200

    Example of sharing lifted state between two components

--- src/js/components/temperature/TemperatureCalculator.js
+++ src/js/components/temperature/TemperatureCalculator.js
@@ -1,35 +1,83 @@
 import React from 'react';
 import TemperatureInput from './TemperatureInput.js';
 
+const scaleNames = {
+    c : "Celsius",
+    f : "Fahrenheit"
+}
+
 export default class Calculator extends React.Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
-            temperature : ''
+            temperatureCel : '',
+            temperaturefahr : '',
+            scale : ''
         }
 
         this.handleTemperatureChange = this.handleTemperatureChange.bind(this);
+        this.convertToCel = this.convertToCel.bind(this);
+        this.convertToFarh = this.convertToFarh.bind(this);
+        this.tryConvert = this.tryConvert.bind(this);
     }
 
-    handleTemperatureChange(temperature) {
-        this.setState({temperature});
-        //console.log("the state is ", this.state);
+    handleTemperatureChange(temperature, scale) {
+        //this.setState({temperatureCel : temperature, scale});
+        if( scale === 'f'){
+            let ct =  this.tryConvert(temperature, this.convertToCel);
+            this.setState({
+                temperatureCel : ct,
+                temperaturefahr: temperature,
+                scale
+            });
+            
+        } else {
+            let ft = this.tryConvert(temperature, this.convertToFarh);
+            this.setState({
+                temperatureCel : temperature,
+                temperaturefahr: ft,
+                scale
+            })
+        }
+    }
+
+    convertToCel(temperature){
+        return (temperature - 32) * 5 / 9;
+    }
+
+    convertToFarh(temperature){
+         return (temperature * 9 / 5) + 32;
+    }
+
+    tryConvert(temperature, convert){
+        const input = parseFloat(temperature);
+        if(Number.isNaN(input)) {
+            return '';
+        }
+        const output = convert(input);
+        const rounded = Math.round(output * 1000) / 1000;
+        return rounded.toString();
     }
     
     render() {
 
-        const {temperature} = this.state;
+        const {temperatureCel, temperaturefahr, scale} = this.state;
+
         return (
-            <div>
+            <div>  
                 <TemperatureInput
-                    temperature = {temperature}
+                    scale = {'c'}
+                    temperature = {temperatureCel}
                     onTemperatureChange = {this.handleTemperatureChange}
                 />
-                <div>
-                    <p> The state in Calculator function has temperature set to: {temperature} </p> 
-                </div>
+                <TemperatureInput
+                    scale = {'f'}
+                    temperature = {temperaturefahr}
+                    onTemperatureChange = {this.handleTemperatureChange}
+                />
+                
             </div>
         );
     }
--- src/js/components/temperature/TemperatureInput.js
+++ src/js/components/temperature/TemperatureInput.js
@@ -1,14 +1,19 @@
 import React from 'react';
 //import Calculator from './TemperatureCalculator.js';
 
+const scaleNames = {
+    c : "Celsius",
+    f : "Fahrenheit"
+}
+
 export default function TemperatureInput(props) {
 
     return (
         <fieldset>
-            <legend> Enter temperature </legend>
+            <legend> Enter temperature in {scaleNames[props.scale]} </legend>
             <input
                 value = {props.temperature}
-                onChange = { (e) => props.onTemperatureChange( e.target.value)}
+                onChange = { (e) => props.onTemperatureChange( e.target.value, props.scale)}
             />
         </fieldset>
     );
